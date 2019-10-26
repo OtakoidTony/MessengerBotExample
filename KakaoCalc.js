d8e8e2d491175e5b9c51facb0d98dbc99932e6f1
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-10-26 15:51:35 +0900

    Update KakaoCalc.js

--- KakaoCalc.js
+++ KakaoCalc.js
@@ -1,1 +1,32 @@
+String.prototype.replaceAll = function(org, dest) {
+    return this.split(org).join(dest);
+}
 
+factorial = function(input) {
+    var temp=1;
+    var output=1;
+    while (temp<=input){
+        output=output*temp;
+        temp=temp+1;
+    }
+    return output
+}
+
+
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
+    if (msg.charAt(0) == "계" && msg.charAt(1) == "산") {
+        var calc=0;
+        if(msg.indexOf("\'")    ==-1 && msg.indexOf("\"")     ==-1&&
+           msg.indexOf("Api")   ==-1 && msg.indexOf("Utils")  ==-1&&
+           msg.indexOf("Log")   ==-1 && msg.indexOf("Appdata")==-1&&
+           msg.indexOf("Data")  ==-1 %% msg.indexOf("Bridge") ==-1&&
+           msg.indexOf("Device")==-1 && msg.indexOf("File")   ==-1&&
+           msg.indexOf("var")   ==-1 && msg.indexOf("?")      ==-1){
+            replier.reply("Exploit Detect");
+        }else{
+            eval("calc="+msg.split(" ", 2)[1]);
+            replier.reply(calc);
+        }
+    }
+}
+        
