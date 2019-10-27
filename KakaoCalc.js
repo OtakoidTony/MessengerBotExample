aba7f9f66b8cd573beb2386082a4fc1ed6986e87
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-10-27 10:48:39 +0900

    Update KakaoCalc.js

--- KakaoCalc.js
+++ KakaoCalc.js
@@ -3,21 +3,30 @@ String.prototype.replaceAll = function(org, dest) {
 }
 
 factorial = function(input) {
-    var temp  =1;
-    var output=1;
-    while(temp<=input){
-        output = output*temp;
-        temp   = temp+1;
+    var temp = 1;
+    var output = 1;
+    while (temp <= input) {
+        output = output * temp;
+        temp = temp + 1;
     }
     return output;
 }
 
 exp = function(input) {
-    var temp=0;
-    var output=0;
-    while (temp<=20){
-        output=((input**temp)/factorial(temp))+output;
-        temp=temp+1;
+    var temp = 0;
+    var output = 0;
+    while (temp <= 20) {
+        output = (java.lang.Math.pow(input, temp) / factorial(temp)) + output;
+        temp = temp + 1;
+    }
+    return output;
+}
+sin = function(input){
+    var temp = 0;
+    var output = 0;
+    while (temp <= 20) {
+        output = (java.lang.Math.pow(-1, temp)*java.lang.Math.pow(input, 2*temp) / factorial(2*temp)) + output;
+        temp = temp + 1;
     }
     return output;
 }
@@ -25,14 +34,15 @@ exp = function(input) {
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
     if (msg.charAt(0) == "계" && msg.charAt(1) == "산") {
         var calc = 0;
-        if(msg.indexOf("\'")    ==-1 && msg.indexOf("\"")     ==-1&&
-           msg.indexOf("Api")   ==-1 && msg.indexOf("Utils")  ==-1&&
-           msg.indexOf("Log")   ==-1 && msg.indexOf("Appdata")==-1&&
-           msg.indexOf("Data")  ==-1 && msg.indexOf("Bridge") ==-1&&
-           msg.indexOf("Device")==-1 && msg.indexOf("File")   ==-1&&
-           msg.indexOf("var")   ==-1 && msg.indexOf("?")      ==-1){
+        if (msg.indexOf("\'") == -1 && msg.indexOf("\"") == -1 &&
+            msg.indexOf("Api") == -1 && msg.indexOf("Utils") == -1 &&
+            msg.indexOf("Log") == -1 && msg.indexOf("Appdata") == -1 &&
+            msg.indexOf("Data") == -1 && msg.indexOf("Bridge") == -1 &&
+            msg.indexOf("Device") == -1 && msg.indexOf("File") == -1 &&
+            msg.indexOf("var") == -1 && msg.indexOf("?") == -1 &&
+            msg.indexOf("java") == -1) {
             replier.reply(eval(msg.split(" ", 2)[1]));
-        }else{
+        } else {
             replier.reply("Exploit Detect");
         }
     }
