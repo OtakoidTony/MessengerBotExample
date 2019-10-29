5cac8e406afb07f5c5e2977b5663c9c2dbcd9022
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-10-29 13:19:33 +0900

    Update KakaoEnc.js

--- KakaoEnc.js
+++ KakaoEnc.js
@@ -1,1 +1,4 @@
-
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
+    if(msg.substring(0,3)=="enc!"){
+        var input=msg.substring(4,msg.length-1);
+        
