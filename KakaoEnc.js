502538bd1c7800a2fd6220bf273841b9bc945f4d
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-10-29 17:53:01 +0900

    Update KakaoEnc.js

--- KakaoEnc.js
+++ KakaoEnc.js
@@ -1,3 +1,21 @@
+charToCode = function(str) {
+  if (!str) return false; // Escaping if not exist
+  var unicode = '';
+  for (var i = 0, l = str.length; i < l; i++) {
+    unicode += '\\' + str[i].charCodeAt(0).toString(2);
+  };
+  return unicode;
+}
+
+charToUnicode = function(str) {
+  if (!str) return false; // Escaping if not exist
+  var unicode = '';
+  for (var i = 0, l = str.length; i < l; i++) {
+    unicode += '\\' + str[i].charCodeAt(0).toString(16);
+  };
+  return unicode;
+}
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
     if(msg.substring(0,3)=="enc!"){
         var input=msg.substring(4,msg.length-1);
