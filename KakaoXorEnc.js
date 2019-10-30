4137e113a273333c9f7769e37d855e4dec52ea0e
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-10-30 19:50:14 +0900

    Update KakaoXorEnc.js

--- KakaoXorEnc.js
+++ KakaoXorEnc.js
@@ -14,18 +14,24 @@ stringToUnicode = function(str) {
     return test;
 }
 
-stringXor = function(str){
-
+stringXor = function(str, password){
+    output = new Array;
+    for (var i = 0, l = str.length; i < l; i++) {
+        ascii = str[i];
+        output.push(ascii^password[i%password.length]);
+    }
+    return output;
+}
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
     if (msg.substring(0, 4) == "enc!") {
         var input = msg.substring(4, msg.length);
-        var encoded = unicodeArrayToBrailleString(stringToUnicode(input));
+        var encoded = stringXor(input, "test");
         replier.reply(encoded);
     }
     if (msg.substring(0, 4) == "dec!") {
         var input = msg.substring(4, msg.length);
-        var decoded = unicodeArrayToString(brailleToHex(input));
+        var decoded = stringXor(input, "test");
         replier.reply(decoded);
     }
 }
