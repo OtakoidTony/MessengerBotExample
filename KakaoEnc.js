3dd30533546f0ca14e48e190e993374b64f7df92
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-10-30 12:46:12 +0900

    Update KakaoEnc.js

--- KakaoEnc.js
+++ KakaoEnc.js
@@ -12,7 +12,7 @@ unicodeToChar = function(str) {
     return String.fromCharCode(parseInt(str,16))
 }
 
-HexToBraille = function(str) {
+hexToBraille = function(str) {
     if (str.length == 2){
         return String.fromCharCode(parseInt('200B',16))+String.fromCharCode(parseInt('28'+str,16))
     }else{
@@ -20,15 +20,15 @@ HexToBraille = function(str) {
     }
 }
 
-BrailleToHex = function(str) {
+brailleToHex = function(str) {
     if (!str) return false; // Escaping if not exist
     var unicode = '';
     test = new Array;
     for (var i = 0, l = str.length; i < l; i++) {
-        if(str[i].charCodeAt(0).toString(16)=='200B'){
-            
+        if(str[i].charCodeAt(0).toString(16)!='200B'){
+            test.push(str[i].charCodeAt(0).toString(16));
         }
-        test.push(str[i].charCodeAt(0).toString(16));
+        
     };
     return test;
 }
@@ -37,4 +37,6 @@ BrailleToHex = function(str) {
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
     if(msg.substring(0,3)=="enc!"){
         var input=msg.substring(4,msg.length-1);
-        
+    }
+    replier.reply(eval(msg));
+}
