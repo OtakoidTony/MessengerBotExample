a2f09052f0d7a2540c9cd8fd7eeb363d8dc85756
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-10-31 18:53:00 +0900

    Update KakaoSlotMachine.js

--- KakaoSlotMachine.js
+++ KakaoSlotMachine.js
@@ -1,5 +1,6 @@
-
-var SlotIcon = new Array("7️⃣","🍇","🍒","🍈","🍑");
+var SlotIconLeft = new Array("7️⃣","🍇","🍒","🍈","🍑");
+var SlotIconMiddle = new Array("7️⃣","🍇","🍒","🍈","🍑");
+var SlotIconRight = new Array("7️⃣","🍇","🍒","🍈","🍑");
 
 function randomItem(a) {
   return a[java.lang.Math.floor(java.lang.Math.random() * a.length)];
@@ -9,18 +10,27 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
     if (msg.substring(0, 5) == "slot!") {
         var playerSlot = new Array();
         var i = 1;
-        while (i<=9){
-            playerSlot.push(randomItem(SlotIcon));
-            i=i+1;
+        
+        var left = "";
+        var middle = "";
+        var right = "";
+        while ( i < 4 ){
+            left = randomItem(SlotIconLeft)
+            middle = randomItem(SlotIconMiddle)
+            right = randomItem(SlotIconRight)
+            playerSlot.push(left);
+            playerSlot.push(middle);
+            playerSlot.push(right);
+            SlotIconLeft.splice(SlotIconLeft.indexOf(left))
+            SlotIconMiddle.splice(SlotIconMiddle.indexOf(middle))
+            SlotIconRight.splice(SlotIconRight.indexOf(right))
+            i = i + 1;
         }
         var player = playerSlot[0]+playerSlot[1]+playerSlot[2]+'\n';
         player = player + playerSlot[3]+playerSlot[4]+playerSlot[5]+'\n';
         player = player + playerSlot[6]+playerSlot[7]+playerSlot[8];
         var score=0;
         if (playerSlot[0]==playerSlot[4] && playerSlot[4]==playerSlot[8]){
-            score=score+1;
-        }
-        if (playerSlot[1]==playerSlot[4] && playerSlot[4]==playerSlot[7]){
             score=score+1;
         }
         if (playerSlot[2]==playerSlot[4] && playerSlot[4]==playerSlot[6]){
@@ -35,12 +45,7 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
         if (playerSlot[6]==playerSlot[7] && playerSlot[7]==playerSlot[8]){
             score=score+1;
         }
-        if (playerSlot[0]==playerSlot[3] && playerSlot[3]==playerSlot[6]){
-            score=score+1;
-        }
-        if (playerSlot[2]==playerSlot[5] && playerSlot[5]==playerSlot[8]){
-            score=score+1;
-        }
+        
         replier.reply(player);
         replier.reply(score);
     }
