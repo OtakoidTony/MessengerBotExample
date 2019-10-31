58960c430973c71df8c913e69aaa3c64a47f5c2b
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-10-31 18:14:46 +0900

    Update KakaoXorEnc.js

--- KakaoXorEnc.js
+++ KakaoXorEnc.js
@@ -23,15 +23,26 @@ stringXor = function(str, password){
     return output;
 }
 
+/* ===============[ http://evalonlabs.com/2015/12/10/Simple-XOR-Encryption-in-JS/ ]=============== */
+function xorConvert (text, key) {
+    var kL = key.length;
+    return Array.prototype
+        .slice.call(text)
+        .map(function (c, index) {
+            return String.fromCharCode(c.charCodeAt(0) ^ key[index % kL].charCodeAt(0));
+        }).join('');
+}
+/* =============================================================================================== */
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
     if (msg.substring(0, 4) == "enc!") {
         var input = msg.substring(4, msg.length);
-        var encoded = stringXor(input, "test");
+        var encoded = xorConvert(input, "test");
         replier.reply(encoded);
     }
     if (msg.substring(0, 4) == "dec!") {
         var input = msg.substring(4, msg.length);
-        var decoded = stringXor(input, "test");
+        var decoded = xorConvert(input, "test");
         replier.reply(decoded);
     }
 }
