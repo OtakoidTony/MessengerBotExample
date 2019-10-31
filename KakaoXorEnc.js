e4f2461a8f011bfb2d57168fea37b57f24ff4fcc
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-10-31 18:17:11 +0900

    Update KakaoXorEnc.js

--- KakaoXorEnc.js
+++ KakaoXorEnc.js
@@ -1,28 +1,3 @@
-stringToUnicode = function(str) {
-    if (!str) return false; // Escaping if not exist
-    var unicode = '';
-    test = new Array;
-    var ascii = '';
-    for (var i = 0, l = str.length; i < l; i++) {
-        ascii = str[i].charCodeAt(0).toString(16);
-        if (ascii.length == 2) {
-            test.push("00" + ascii);
-        } else {
-            test.push(ascii);
-        }
-    };
-    return test;
-}
-
-stringXor = function(str, password){
-    output = new Array;
-    for (var i = 0, l = str.length; i < l; i++) {
-        ascii = str[i];
-        output.push(ascii^password[i%password.length]);
-    }
-    return output;
-}
-
 /* ===============[ http://evalonlabs.com/2015/12/10/Simple-XOR-Encryption-in-JS/ ]=============== */
 function xorConvert (text, key) {
     var kL = key.length;
