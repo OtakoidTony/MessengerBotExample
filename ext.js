6db69d9ee2f9c4d5f0b6257192d80d2e9983097c
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-03 12:03:22 +0900

    Update ext.js

--- ext.js
+++ ext.js
@@ -9,3 +9,20 @@ sigma = function(inf, sup, expression){
     return output;
 }
 
+quadEqation = function(a, b, c){
+    var output="";
+    var D=0;
+    D=(b*b)-(4*a*c);
+    var i=2;
+    var Dsqare=1;
+    while (i<=Math.Pow(D,0.5)){
+        if(D%(i*i)==0){
+            D=D/(i*i);
+            Dsqare=Dsqare*i;
+        }
+        i=i+1;
+    }
+    Log.info(D);
+    Log.info(Dsqare);
+}
+
