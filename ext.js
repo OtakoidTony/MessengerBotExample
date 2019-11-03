4a142473817dafd110e231efaa50a83c76840fe0
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-03 12:21:47 +0900

    Update ext.js

--- ext.js
+++ ext.js
@@ -9,13 +9,15 @@ sigma = function(inf, sup, expression){
     return output;
 }
 
+
+
 quadEqation = function(a, b, c){
     var output="";
     var D=0;
     D=(b*b)-(4*a*c);
     var i=2;
     var Dsqare=1;
-    while (i<=Math.Pow(D,0.5)){
+    while (i<=java.lang.Math.pow(D,0.5)){
         if(D%(i*i)==0){
             D=D/(i*i);
             Dsqare=Dsqare*i;
@@ -24,5 +26,27 @@ quadEqation = function(a, b, c){
     }
     Log.info(D);
     Log.info(Dsqare);
+    // (-b±√b²-4ac)/2a
+    var output1=0;
 }
 
+makeQuadEqation = function(a,b){
+    var A=-(a+b);
+    var B=a*b;
+    var Astr="";
+    var Bstr="";
+    if (A>0){
+        Astr="+"+A.toString(10)+"𝒙";
+    }
+    if (A<0){
+        Astr=A.toString(10)+"𝒙";
+    }
+    if (B>0){
+        Bstr="+"+B.toString(10);
+    }
+    if (B<0){
+        Bstr=B.toString(10);
+    }
+    return "𝒙²"+Astr+Bstr;
+}
+    
