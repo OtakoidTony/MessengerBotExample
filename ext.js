c284d113050be1d4a70776584adb560d2b496ce4
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-03 11:44:33 +0900

    Update ext.js

--- ext.js
+++ ext.js
@@ -1,4 +1,10 @@
 
 sigma = function(inf, sup, expression){
     var temp=inf;
+    var output=0;
     while (temp<=sup){
+        output=output+expression;
+        temp=temp+1;
+    }
+    return output;
+}
