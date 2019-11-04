fad4e0e23ecc761e699d5e93fdce6f7bbddf8b2a
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-04 21:40:17 +0900

    Update KakaoAiChat.js

--- KakaoAiChat.js
+++ KakaoAiChat.js
@@ -2,10 +2,22 @@ function randomItem(a) {
     return a[java.lang.Math.floor(java.lang.Math.random() * a.length)];
 }
 
-const msgDict = {
-    'Hello': "Hello World!",
-    'loli is': "LIFE!!!",
-    'XD': "lol"
+var msgDict = {
+    '안녕하세요': [
+        "안녕하세여~!",
+        "ㅎㅇㅎㅇ~!",
+        "어서와여~!",
+        "ㅎㅇ에여~!"
+    ],
+    '냥!': [
+        "냐아앙~!",
+        "냥~! 냥~!",
+        "냐아~!"
+    ],
+    '박제': [
+        "ㅋㅋㅋㅋㅋㅋ",
+        "증! 거! 확! 보!"
+    ]
 };
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
