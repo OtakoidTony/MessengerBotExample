f73ac10526b462eb8383b344b3b3e67248ed7b18
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-04 15:48:05 +0900

    Update KakaoNsfwDetect.js

--- KakaoNsfwDetect.js
+++ KakaoNsfwDetect.js
@@ -1,1 +1,34 @@
+Utils.getPostFromWeb = function(url, parameters) {
+    try {
+        var url = new java.net.URL(url);
+        var con = url.openConnection();
+        
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", "\'User-Agent\': \'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\'");
+        con.setDoOutput(true);
+        if (con != null) {
+            con.setConnectTimeout(5000);
+            con.setUseCaches(false);
+            var wr = new java.io.DataOutputStream(con.getOutputStream());
+            wr.writeBytes(parameters);
+            wr.flush();
+            wr.close();
+            var responseCode = con.getResponseCode();
+            var isr = new java.io.InputStreamReader(con.getInputStream());
+            var br = new java.io.BufferedReader(isr);
+            var inputLine;
+            var response = new java.lang.StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            con.close();
+            return response.toString();
+        }
+    }
+}
 
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
+    var url = 'data:image/jpeg;base64,'+ImageDB.getPicture();
+    
+}
