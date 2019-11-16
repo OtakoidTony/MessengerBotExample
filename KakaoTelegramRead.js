704fc864953363a7529de7217ee6a9aa9f7f5bc9
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-16 19:52:38 +0900

    Update KakaoTelegramRead.js

--- KakaoTelegramRead.js
+++ KakaoTelegramRead.js
@@ -1,6 +1,6 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
     if(msg.substring(0, 5) == "send!"){
         var message = msg.substring(6, msg.length);
-        var data = Utils.getWebText("https://api.telegram.org/bot"+ token +"/sendMessage?chat_id="+ chat_id +"&text="+sender+": "+message);
+        var data = Utils.getWebText("https://api.telegram.org/bot"+token+"/getUpdates");
     }
 }
