c3e4059f39eacdecbf6e7ee053310cb544646b6f
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-21 19:57:06 +0900

    Update KakaoCos.js

--- KakaoCos.js
+++ KakaoCos.js
@@ -1,3 +1,4 @@
+/*문장 내 띄어쓰기를 기준으로한 단어별 갯수 */
 function numberOf(sentence){
     var wordList = sentence.split(" ");
     var numberList = {};
@@ -12,8 +13,20 @@ function numberOf(sentence){
     return numberList;
 }
 
+/* sentence에서 word가 출현되는 횟수 */
 function inSentence(sentence, word){
     var wordList = sentence.split(word);
-    return wordList.length-1
+    return wordList.length-1;
 }
 
+/* 단어 뭉치 */
+var wordKind = ["로리", "수학", "공학", "논리", "법령", "근거", "조사", ,,, ];
+
+function extractSize(sentence){
+    var number = {};
+    for ( var i in wordKind ){
+        number[wordKind[i]]=inSentence(sentence, wordKind[i]);
+    }
+    return number;
+}
+        
