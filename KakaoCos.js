a7bfff82bdb2f9bfcea51e4219167e5f3d24cd30
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-21 19:41:56 +0900

    Update KakaoCos.js

--- KakaoCos.js
+++ KakaoCos.js
@@ -1,2 +1,19 @@
-function numberOf(word){
-    
+function numberOf(sentence){
+    var wordList = sentence.split(" ");
+    var numberList = {};
+    for (var i in wordList){
+        numberList[wordList[i]] = 0;
+    }
+    var j = 0;
+    while (j < wordList.length){
+        numberList[wordList[j]]=numberList[wordList[j]]+1;
+        j=j+1;
+    }
+    return numberList;
+}
+
+function inSentence(sentence, word){
+    var wordList = sentence.split(word);
+    return wordList.length-1
+}
+
