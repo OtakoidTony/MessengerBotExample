d014d72032390ebb91a02f372bedf27b8d8d965e
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-21 22:24:43 +0900

    Update KakaoRSA.js

--- KakaoRSA.js
+++ KakaoRSA.js
@@ -1,4 +1,5 @@
-modinv = function (a, m) {
+// https://github.com/alexvanmaele/RSA-Prime-Calculator
+modinv = function(a, m) {
     var v = 1;
     var d = a;
     var u = (a == 1);
@@ -22,60 +23,65 @@ modinv = function (a, m) {
     return u;
 }
 
+// https://www.w3resource.com/javascript-exercises/javascript-math-exercise-8.php
 function gcd(x, y) {
-  if ((typeof x !== 'number') || (typeof y !== 'number')) 
-    return false;
-  x = Math.abs(x);
-  y = Math.abs(y);
-  while(y) {
-    var t = y;
-    y = x % y;
-    x = t;
-  }
-  return x;
-}
-
-function isPrime(n, k) { 
-    // Corner cases 
-    if (n <= 1 || n == 4) return false; 
-    if (n <= 3) return true; 
-      
-    // Try k times 
-    var a = 0;
-    while (k > 0) {       
-        // Pick a randomly in the range [2, n − 2]
-        a = 2 + Math.floor(Math.random() * (n - 4));        
-        // Fermat's little theorem 
-        if (Math.pow(a, n - 1)%n != 1) {
-            return Math.pow(a, n - 1)%n; 
-        }
-        k=k-1;
+    if ((typeof x !== 'number') || (typeof y !== 'number'))
+        return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while (y) {
+        var t = y;
+        y = x % y;
+        x = t;
     }
-    return true; 
+    return x;
 }
 
-function get_private_key(e, tot){
-    var k=1;
-    while (((e*k)%tot) != 1 || k == e){
-        k+=1;
+function isPrime(n, k) {
+    // Corner cases 
+    if (n <= 1 || n == 4) return false;
+    if (n <= 3) return true;
+    var a = 0;
+    while (k > 0) {
+        // Pick a randomly in the range [2, n − 2]
+        a = 2 + Math.floor(Math.random() * (n - 4));
+        // Fermat's little theorem 
+        if (Math.pow(a, n - 1) % n != 1) {
+            return Math.pow(a, n - 1) % n;
+        }
+        k = k - 1;
+    }
+    return true;
+}
+
+function totient(p, q) {
+    return (p - 1) * (q - 1);
+}
+
+/*
+RSA 알고리즘 개념 및 구현(python)
+https://wkdtjsgur100.github.io/RSA-algorithm/
+*/
+function get_private_key(e, tot) {
+    var k = 1;
+    while (((e * k) % tot) != 1 || k == e) {
+        k += 1;
     }
     return k;
 }
 
-function get_public_key(totient){
-    var e=2
-    while (e< totient && gcd(e, totient)!=1){
+/* 
+RSA 알고리즘 개념 및 구현(python)
+https://wkdtjsgur100.github.io/RSA-algorithm/
+*/
+function get_public_key(totient) {
+    var e = 2;
+    while (e < totient && gcd(e, totient) != 1) {
         e += 1;
     }
     return e;
 }
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
-    var i=1000000;
-    while(i<=1000000000){
-        if (isPrime(i,10)){
-            replier.reply(i);
-        }
-        i=i+1;
-    }
+    
 }
