089b9a18a0970dfb48a135e1e2cb4ee188ca2fc4
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-22 17:59:52 +0900

    Update KakaoANN.js

--- KakaoANN.js
+++ KakaoANN.js
@@ -1,35 +1,53 @@
-// https://aljjabaegi.tistory.com/391
-function sumMatrix(A,B){
+const Matrix={};
+
+Matrix.sum = function (A, B) {
     var answer = Array();
-    for(var i=0; i<A.length; i++){
+    for (var i = 0; i < A.length; i++) {
         answer[i] = [];
-        for(var j=0; j<A[0].length; j++){
-            answer[i][j] = A[i][j]+B[i][j];
+        for (var j = 0; j < A[0].length; j++) {
+            answer[i][j] = A[i][j] + B[i][j];
         }
     }
     return answer;
 }
+Matrix.multiply = function (a, b) {
+    var aNumRows = a.length,
+        aNumCols = a[0].length,
+        bNumRows = b.length,
+        bNumCols = b[0].length,
+        m = new Array(aNumRows); // initialize array of rows
+    for (var r = 0; r < aNumRows; ++r) {
+        m[r] = new Array(bNumCols); // initialize the current row
+        for (var c = 0; c < bNumCols; ++c) {
+            m[r][c] = 0; // initialize the current cell
+            for (var i = 0; i < aNumCols; ++i) {
+                m[r][c] += a[r][i] * b[i][c];
+            }
+        }
+    }
+    return m;
+}
 
-const random={};
+const random = {};
 
-random.rand = function(a){
-    var q=[];
-    var i=0;
-    while (i<a){
+random.rand = function(a) {
+    var q = [];
+    var i = 0;
+    while (i < a) {
         q.push(Math.random());
-        i=i+1;
+        i = i + 1;
     }
     return q;
 }
 
-function sigmoid(x){
+function sigmoid(x) {
     return 1 / (1 + Math.exp(-x));
 }
 
-function ReLU(x){
-    if (x>=0){
+function ReLU(x) {
+    if (x >= 0) {
         return x;
-    }else{
+    } else {
         return 0;
     }
 }
