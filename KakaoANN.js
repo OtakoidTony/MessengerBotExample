c6722e56690112397c60a7accb2394dbdfb24a8b
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-23 12:45:03 +0900

    Update KakaoANN.js

--- KakaoANN.js
+++ KakaoANN.js
@@ -1,4 +1,4 @@
-const Matrix = {};
+var Matrix = {};
 
 Matrix.sum = function(A, B) {
     var answer = [];
@@ -10,7 +10,7 @@ Matrix.sum = function(A, B) {
     }
     return answer;
 }
-Matrix.multiply = function(a, b) {
+Matrix.dot = function(a, b) {
     var aNumRows = a.length,
         aNumCols = a[0].length,
         bNumRows = b.length,
@@ -48,6 +48,21 @@ Matrix.zeros = function(a, b) {
     }
     return A;
 }
+Matrix.numbers = function(a, b, n) {
+    var i = 0;
+    var j = 0;
+    var A = [];
+    var B = [];
+    while (j < b) {
+        B.push(n);
+        j = j + 1;
+    }
+    while (i < a) {
+        A.push(B);
+        i = i + 1;
+    }
+    return A;
+}
 Matrix.time = function(A, t) {
     var out = [];
     for (var i in A) {
@@ -58,7 +73,7 @@ Matrix.time = function(A, t) {
     }
     return out;
 }
-Matrix.dot = function(A, B) {
+Matrix.multiply_scalar = function(A, B) {
     var out = 0;
     for (var i in A) {
         for (var j in A[i]) {
@@ -75,6 +90,18 @@ Matrix.plus = function(A, B) {
         Output[i] = [];
         for (j in A[i]) {
             Output[i][j] = A[i][j] + B[i][j];
+        }
+    }
+    return Output;
+}
+Matrix.minus = function(A, B) {
+    var i = 0;
+    var j = 0;
+    var Output = [];
+    for (i in A) {
+        Output[i] = [];
+        for (j in A[i]) {
+            Output[i][j] = A[i][j] - B[i][j];
         }
     }
     return Output;
@@ -114,7 +141,7 @@ Matrix.sigmoid_derivative = function(A) {
     }
     return out;
 }
-Matrix.scalar_time = function(A, B) {
+Matrix.multiply = function(A, B) {
     var answer = [];
     for (var i = 0; i < A.length; i++) {
         answer[i] = [];
@@ -124,27 +151,6 @@ Matrix.scalar_time = function(A, B) {
     }
     return answer;
 }
-
-function sigmoid(x) {
-    return 1 / (1 + Math.exp(-x));
-}
-
-function sigmoid_derivative(p) {
-    return p * (1 - p);
-}
-
-const random = {};
-
-random.rand = function(a) {
-    var q = [];
-    var i = 0;
-    while (i < a) {
-        q.push(Math.random());
-        i = i + 1;
-    }
-    return q;
-}
-
 
 function sigmoid(x) {
     return 1 / (1 + Math.exp(-x));
@@ -170,26 +176,21 @@ function NeuralNetwork(x, y) {
     this.output = Matrix.zeros(Matrix.shape(y)[0], Matrix.shape(y)[1]);
 
     this.feedforward = function() {
-        this.layer1 = Matrix.sigmoid(Matrix.multiply(this.input, this.weights1));
-        this.layer2 = Matrix.sigmoid(Matrix.multiply(this.layer1, this.weights2));
+        this.layer1 = Matrix.sigmoid(Matrix.dot(this.input, this.weights1));
+        this.layer2 = Matrix.sigmoid(Matrix.dot(this.layer1, this.weights2));
         return this.layer2
     }
 
     this.backprop = function() {
-        var d_weights2 = Matrix.dot(Matrix.T(this.layer1), Matrix.scalar_time(
-                Matrix.time(Matrix.plus(this.y,Matrix.time(this.output,-1)),2),
-                    Matrix.sigmoid_derivative(this.output)))
-
-        var d_weights1 = Matrix.dot(Matrix.T(this.input), Matrix.scalar_time(Matrix.dot(Matrix.time(Matrix.scalar_time(Matrix.plus(this.y ,Matrix.time(this.output,-1)),sigmoid_derivative(this.output)),2), Matrix.T(this.weights2)),sigmoid_derivative(this.layer1)));
-        /*
-        np.dot(
-            self.input.T,
-            np.dot(
-                time((self.y -self.output),2*sigmoid_derivative(self.output)),
-                self.weights2.T
-            )*sigmoid_derivative(self.layer1)
-        )
-        */
+		var y_minus_output=Matrix.minus(this.y, this.output);
+		var second_run = Matrix.time(y_minus_output, 2);
+		var third_run = Matrix.multiply(second_run, Matrix.sigmoid_derivative(this.output));
+        var d_weights2 = Matrix.dot(Matrix.T(this.layer1), third_run)
+		
+		var run_4 = Matrix.dot(third_run, Matrix.T(this.weights2));
+		var run_5 = Matrix.multiply(run_4, Matrix.sigmoid_derivative(this.layer1));
+        var d_weights1 = Matrix.dot(Matrix.T(this.input),  run_5)
+        
         this.weights1 = Matrix.plus(this.weights1, d_weights1);
         this.weights2 = Matrix.plus(this.weights2, d_weights2);
     }
@@ -201,3 +202,10 @@ function NeuralNetwork(x, y) {
 }
 var X=[[0,0,1],[0,1,1],[1,0,1],[1,1,1]];
 var y=[[0],[1],[1],[0]];
+
+var NN=new NeuralNetwork(X,y);
+var i=0;
+while(i<100){
+    NN.train(X,y);
+    i=i+1;
+}
