6c161ad43245762290297450587ff0779bfbfd0f
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-24 17:05:37 +0900

    Update KakaoANN.js

--- KakaoANN.js
+++ KakaoANN.js
@@ -320,6 +320,40 @@ Utils.getShortUrl = function(url) {
     return JSON.parse(Utils.getShortUrlJson(url)).result;
 }
     
-    
+sendPost(targetUrl, parameters){
+    try {
+        var url = new java.net.URL(targetUrl);
+        var con = url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent",
+            "\'User-Agent\': \'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\'");
+        con.setRequestProperty("Content-Type", "multipart/form-data; boundary=----WebKitFormBoundaryB4yoMQALFribuPdn");
+        con.setRequestProperty("x-requested-with", "XMLHttpRequest");
+        if (con != null) {
+            con.setConnectTimeout(5000);
+            con.setUseCaches(false);
+            con.setDoOutput(true); // POST 파라미터 전달을 위한 설정
+            var wr = new java.io.DataOutputStream(con.getOutputStream());
+            wr.writeBytes(parameters);
+            wr.flush();
+            wr.close();
+            
+            var responseCode = con.getResponseCode();
+            var isr = new java.io.InputStreamReader(con.getInputStream());
+            var br = new java.io.BufferedReader(isr);
+            var str = br.readLine();
+            var line = "";
+            while ((line = br.readLine()) != null) {
+                str += "\n" + line;
+            }
+            isr.close();
+            br.close();
+            con.disconnect();
+        }
+        return str + "";
+    } catch (e) {
+        Log.debug(e);
+    }
+}  
     
     
