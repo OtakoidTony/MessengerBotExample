940e46f7c15a94e1b4e7c57ec5e1a32a53e9a4a6
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-26 22:51:05 +0900

    Update RojikuBot.js

--- RojikuBot.js
+++ RojikuBot.js
@@ -1,1 +1,29 @@
+var sdcard = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
 
+function save(folderName, fileName, str) {
+    var c = new java.io.File(sdcard + "/" + folderName + "/" + fileName);
+    var d = new java.io.FileOutputStream(c);
+    var e = new java.lang.String(str);
+    d.write(e.getBytes());
+    d.close();
+}
+
+function read(folderName, fileName) {
+    var b = new java.io.File(sdcard + "/" + folderName + "/" + fileName);
+    if (!(b.exists())) return null;
+    var c = new java.io.FileInputStream(b);
+    var d = new java.io.InputStreamReader(c);
+    var e = new java.io.BufferedReader(d);
+    var f = e.readLine();
+    var g = "";
+    while ((g = e.readLine()) != null) {
+        f += "\n" + g;
+    }
+    c.close();
+    d.close();
+    e.close();
+    return f.toString();
+}
+
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
+    
