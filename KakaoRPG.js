ff27876620b6e3e20e1551be380061030a9d0047
OtakoidTony <36785562+OtakoidTony@users.noreply.github.com>
2019-11-27 19:19:48 +0900

    Update KakaoRPG.js

--- KakaoRPG.js
+++ KakaoRPG.js
@@ -34,16 +34,18 @@ const game_data_folder = "Game_Data";
 /* 게임 아이템 목록 */
 const GameItem = [
     {
-        '밧줄': "무언가를 묶을 때 사용할 수 있다.",
         '진통제': "아플 때 먹으면 괜찮아 진다.",
+        '밧줄': "무언가를 묶을 때 사용할 수 있다.",
         '손전등': "배터리가 있으면 어두운 곳을 볼 수 있다."
     },
     {
+        '진통제': "아플 때 먹으면 괜찮아 진다.",
         '배터리': "전자기기를 사용할 수 있다.",
         '유리조각': "날카로워서 잘못 만지면 다칠 수 있다.",
         '가위': "문방구에서 살 수 있는 흔한 가위다."
     },
     {
+        '진통제': "아플 때 먹으면 괜찮아 진다.",
         '톱': "실과시간에 쓰던거랑 비슷하게 생긴 톱이다.",
         '열쇠뭉치': "엄청나게 많은 열쇠가 있다.",
         '신분증': "모르는 사람의 신분증이다."
@@ -133,19 +135,24 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
     var WhiteList = new Array("사용할 단톡방");
     if (WhiteList.indexOf(room) != -1 || isGroupChat == false) {
         if (command(msg)[0] == ":start") {
+
+            /* <--------[게임 데이터 생성 시작]--------> */
             var sender_data = new UserData();
             sender_data.init(command(msg)[1]);
             sender_data.save(sender);
+            /* <--------[게임 데이터 생성 완료]--------> */
+
             replier.reply("게임데이터가 생성되었습니다.");
             var sender_meessage_name = "[" + sender_data.data.name + "] ";
 
-            /* 게임 첫 실행 시 스토리. */
+            /* <--------[게임 여는 스토리 실행]--------> */
             replier.reply(sender_meessage_name + "어... 여기는... 어디지?");
             replier.reply(sender_meessage_name + "여기 누구 없어요???");
             replier.reply("주위를 둘러보았지만, 아무도 없었다.");
             replier.reply(sender_meessage_name + "어흐흐흐흫ㅎ흫흙 ㅠㅠ");
             replier.reply("[SYS] " + sender_data.data.name + "는 지금 밀폐된 공간에 갇혀있습니다. 어서 탈출하세요!");
             replier.reply("[SYS] :help를 입력하면 명령어 목록을 확인할 수 있습니다.");
+            /* <--------[게임 여는 스토리 종료]--------> */
         }
         if (msg == ":help") {
             replier.reply(commands_help);
