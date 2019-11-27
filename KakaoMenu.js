4d367b5477dd5eba3acc472b344837e975c7a829
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-27 11:39:17 +0900

    Update KakaoMenu.js

--- KakaoMenu.js
+++ KakaoMenu.js
@@ -1,7 +1,36 @@
+function command(cmd) {
+    var cmd_str = cmd.split(' ')[0];
+    var param = cmd.substring(cmd_str.length + 1, cmd.length);
+    return [cmd_str, param];
+}
+
+var client = [];
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
-    try {
-        if (isGroupChat) {
-            if (!(room in Attendance)) {
-                Attendance[room] = {}; /* 출석부에 없는 채팅방이면 빈 객체를 생성 */
+    if (msg == ":menu") {
+        if (!(sender in client)) {
+            client.push(sender);
+        }
+        replier.reply("[1] Menu 1");
+        replier.reply("[2] Menu 2");
+        replier.reply("[3] Menu 3");
+    }
+    if (command(msg)[0] == ":choice") {
+        if (sender in client) {
+            client.splice(client.indexOf(sender), 1);
+            switch (command(msg)[1]) {
+                case '1':
+                    replier.reply("1을 선택하였습니다.");
+                    break;
+                case '2':
+                    replier.reply("2을 선택하였습니다.");
+                    break;
+                case '3':
+                    replier.reply("3을 선택하였습니다.");
+                    break;
+                default:
+                    replier.reply("다시 선택해 주십시오.");
             }
-            if (Attendance[room][sender] != date()) {
+        }
+    }
+}
