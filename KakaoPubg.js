bf223e69a26ca02d743ec13cbfdc2975e2963b26
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-28 01:01:45 +0900

    Update KakaoPubg.js

--- KakaoPubg.js
+++ KakaoPubg.js
@@ -2,18 +2,12 @@ Utils.getPubg = function() {
     try {
         var url = new java.net.URL("https://pubg.op.gg/api/leaderboard/ranked-users?platform=steam&queue_size=1&mode=tpp&limit=10");
         var con = url.openConnection();
+        con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", "\'User-Agent\': \'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\'");
-        con.setRequestProperty("Server", "nginx");
-        con.setRequestProperty("X-Powered-By", "PHP/7.2.24");
-        con.setRequestProperty("Cache-Control", "no-cache, private");
-        con.setRequestProperty("Content-Encoding", "gzip");
-        con.setRequestProperty("X-Amz-Cf-Pop", "ICN55-C1");
-        con.setRequestProperty("X-Amz-Cf-Id", "oTcEQQRf8_FPqw8tTiXU7mE-pE-4d3YF9TIu-i_xo4wwEf5bkNm2_A==");
-        con.setRequestProperty("Content-Length", "1346");
-        con.setRequestProperty("Date", "Wed, 27 Nov 2019 15:40:07 GMT");
-        con.setRequestProperty("Connection", "keep-alive");
-        con.setRequestProperty("Vary", "Accept-Encoding");
-        con.setRequestProperty("Set-Cookie", "ak_bmsc=7D6A39897F0C7AA1D0B7EF4FF4716410ADDFE39547050000D798DE5D87EA0045~plZyjN0kjuea8DEJ+Mp/DWM6E97JF37Sl5RFIwMPo1jAvG9IdwDqj2VfbnmsrJtnMzfLXWcxiBRRkFIFO1cDVRtEp6LazxbVSirWNgxJgA8k1biOFDnLghX7FeFYkd1uXtnjz1RpbN19Zg7B+Mp0r7LfjsAAxNDQtRVgpa3TFuphd70JWs9rWfoSVYxa0Q3970UtyDvKK/TmN06gL/fcCsVsq5/RC2zGDUpnC2dqi+BtU=; expires=Wed, 27 Nov 2019 17:40:07 GMT; max-age=7200; path=/; domain=.pubg.op.gg; HttpOnly");
+        con.setRequestProperty("accept-encoding", "gzip, deflate, br");
+        con.setRequestProperty("cache-control", "max-age=0");
+        con.setRequestProperty("accept-language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja-JP;q=0.6,ja;q=0.5");
+        con.setRequestProperty("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
         if (con != null) {
             con.setConnectTimeout(5000);
             con.setUseCaches(false);
