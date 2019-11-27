06f220ed37ad611bbbedd159694f156bd2578b70
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-28 01:22:17 +0900

    Update KakaoPubg.js

--- KakaoPubg.js
+++ KakaoPubg.js
@@ -3,11 +3,12 @@ Utils.getPubg = function() {
         var url = new java.net.URL("https://pubg.op.gg/api/leaderboard/ranked-users?platform=steam&queue_size=1&mode=tpp&limit=10");
         var con = url.openConnection();
         con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", "PostmanRuntime/7.20.1");
-        con.setRequestProperty("Accept", "*/*");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36");
+        con.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
         con.setRequestProperty("Cache-Control", "no-cache");
         con.setRequestProperty("Host", "pubg.op.gg");
         con.setRequestProperty("Accept-Encoding", "gzip, deflate");
+        con.setRequestProperty("accept-language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja-JP;q=0.6,ja;q=0.5");
         con.setRequestProperty("Connection", "keep-alive");
         if (con != null) {
             con.setConnectTimeout(5000);
