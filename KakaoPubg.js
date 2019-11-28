ebe09682b66661d4666d75541049da8e15627964
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-11-28 15:22:11 +0900

    Update KakaoPubg.js

--- KakaoPubg.js
+++ KakaoPubg.js
@@ -1,50 +1,60 @@
 Utils.getPubg = function() {
-    doc = org.jsoup.Jsoup.connect("https://pubg.op.gg/api/leaderboard/ranked-users?platform=steam&queue_size=1&mode=tpp&limit=10")
-        .userAgent("Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36")
-        .header("content-encoding", "gzip")
-        .header("content-length", "1351")
-        .header("date", "Wed, 27 Nov 2019 15:54:49 GMT")
-        .header("content-encoding", "gzip")
-
-
-        .header("accept-language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja-JP;q=0.6,ja;q=0.5")
-        .header("cache-control", "max-age=0")
-        .header("dnt", "1")
-        .header("upgrade-insecure-requests", "1")
-        .header("authority", "pubg.op.gg")
-        .header("path", "/api/leaderboard/ranked-users?platform=steam&queue_size=1&mode=tpp&limit=10")
-        .header("scheme", "https")
-        .header("Connection", "keep-alive")
-        .header("sec-fetch-mode", "navigate")
-        .header("sec-fetch-site", "none")
-        .header("sec-fetch-user", "?1")
-        .header("upgrade-insecure-requests", "1")
-
-        .ignoreContentType(true)
-        .get();
-    return doc;
+    try {
+        var url = new java.net.URL("https://pubg.op.gg/api/leaderboard/ranked-users?platform=steam&queue_size=1&mode=tpp&limit=10");
+        var con = url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        if (con != null) {
+            con.setConnectTimeout(5000);
+            con.setUseCaches(false);
+            var isr = new java.io.InputStreamReader(con.getInputStream());
+            var br = new java.io.BufferedReader(isr);
+            var str = br.readLine();
+            var line = "";
+            while ((line = br.readLine()) != null) {
+                str += "\n" + line;
+            }
+            isr.close();
+            br.close();
+            con.disconnect();
+        }
+        var result = str + "";
+        return JSON.parse(result).items;
+    } catch (e) {
+        Log.debug(e);
+    }
 }
 
-doc = org.jsoup.Jsoup.connect("https://pubg.op.gg/api/leaderboard/ranked-users?platform=steam&queue_size=1&mode=tpp&limit=10")
-    .userAgent("Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36")
-    .header("content-encoding", "gzip")
-    .header("content-length", "1351")
-    .header("date", "Wed, 27 Nov 2019 15:54:49 GMT")
-    .header("content-encoding", "gzip")
-
-
-    .header("accept-language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja-JP;q=0.6,ja;q=0.5")
-    .header("cache-control", "max-age=0")
-    .header("dnt", "1")
-    .header("upgrade-insecure-requests", "1")
-    .header("authority", "pubg.op.gg")
-    .header("path", "/api/leaderboard/ranked-users?platform=steam&queue_size=1&mode=tpp&limit=10")
-    .header("scheme", "https")
-    .header("Connection", "keep-alive")
-    .header("sec-fetch-mode", "navigate")
-    .header("sec-fetch-site", "none")
-    .header("sec-fetch-user", "?1")
-    .header("upgrade-insecure-requests", "1")
-
-    .ignoreContentType(true)
-    .get();
+/*
+items
+items.offset
+items.user
+items.user.nickname
+items.user.profile_url
+items.ranked_stats
+items.ranked_stats.grade
+items.ranked_stats.tier
+items.ranked_stats.tier.title
+items.ranked_stats.tier.short_title
+items.ranked_stats.tier.image_url
+items.ranked_stats.stats
+items.ranked_stats.stats.matches_cnt
+items.ranked_stats.stats.win_matches_cnt
+items.ranked_stats.stats.topten_matches_cnt
+items.ranked_stats.stats.kills_sum
+items.ranked_stats.stats.kills_max
+items.ranked_stats.stats.assists_sum
+items.ranked_stats.stats.headshot_kills_sum
+items.ranked_stats.stats.deaths_sum
+items.ranked_stats.stats.longest_kill_max
+items.ranked_stats.stats.rank_avg
+items.ranked_stats.stats.damage_dealt_avg
+items.ranked_stats.stats.time_survived_avg
+items.ranked_stats.stats.rank_points
+items.ranked_stats.stats.best_rank_point
+items.ranked_stats.stats.rank_point_title
+items.ranked_stats.ranks
+items.ranked_stats.ranks.rank_points
+items.ranked_stats.max_ranks
+items.ranked_stats.max_ranks.rank_points
+items.ranked_stats.lone_survivor_flag
+*/
