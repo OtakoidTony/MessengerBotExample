f965ff6d7810bf573bb63def1a5760462f447351
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-12-02 20:20:46 +0900

    Update KakaoLinear.js

--- KakaoLinear.js
+++ KakaoLinear.js
@@ -1,1 +1,20 @@
-
+function method_of_least_squares(p){
+    var sigma_x1 = 0;
+    var sigma_x2 = 0;
+    var sigma_y1 = 0;
+    var sigma_xy = 0;
+    var n = p.length;
+    var i = 0;
+    while ( i < n ){
+        sigma_x1 = sigma_x1 + p[i][0];
+        sigma_x2 = sigma_x2 + ( p[i][0] * p[i][0] );
+        sigma_y1 = sigma_y1 + p[i][1];
+        sigma_xy = sigma_xy + ( p[i][0] * p[i][1] );
+        i = i + 1;
+    }
+    
+    var b = ((sigma_y1*sigma_x1)-(n*sigma_xy))/((sigma_x1*sigma_x1)-(n*sigma_x2));
+    var a = (sigma_y1-(b*sigma_x1))/n;
+    return [a, b];
+}
+    
