be1cdce3f4e6b31f574e0943a72190234cf27a9e
박현 <36785562+OtakoidTony@users.noreply.github.com>
2019-12-03 17:54:25 +0900

    Update KakaoPoint.js

--- KakaoPoint.js
+++ KakaoPoint.js
@@ -1,1 +1,30 @@
-function 
+var sdcard = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+
+function randomItem(a) {
+    return a[Math.floor(Math.random() * a.length)];
+}
+
+function save(folderName, fileName, str) {
+    var c = new java.io.File(sdcard + "/" + folderName + "/" + fileName);
+    var d = new java.io.FileOutputStream(c);
+    var e = new java.lang.String(str);
+    d.write(e.getBytes());
+    d.close();
+}
+
+function read(folderName, fileName) {
+    var b = new java.io.File(sdcard + "/" + folderName + "/" + fileName);
+    if (!(b.exists())) return null;
+    var c = new java.io.FileInputStream(b);
+    var d = new java.io.InputStreamReader(c);
+    var e = new java.io.BufferedReader(d);
+    var f = e.readLine();
+    var g = "";
+    while ((g = e.readLine()) != null) {
+        f += "\n" + g;
+    }
+    c.close();
+    d.close();
+    e.close();
+    return f.toString();
+}
