db9476ba631f907d6742ec68da19343b3c55f665
OtakoidTony <36785562+OtakoidTony@users.noreply.github.com>
2019-12-04 12:49:01 +0900

    Update KakaoHitomi.js

--- KakaoHitomi.js
+++ KakaoHitomi.js
@@ -1,64 +1,74 @@
+String.prototype.replaceAll = function (org, dest) {
+    return this.split(org).join(dest);
+}
+
+/**
+ * 
+ * @param {string} cmd
+ */
+function command(cmd) {
+    var cmd_str = cmd.split(' ')[0];
+    var param = cmd.substring(cmd_str.length + 1, cmd.length);
+    return [cmd_str, param];
+}
+
+/**
+ * 
+ * @param {string} gallery_id
+ */
+function Hitomi(gallery_id) {
+    var input_url = 'https://hitomi.la/galleries/' + gallery_id + ".html";
+    var data = Utils.getWebText(input_url);
+    /* --[ hitomi.la href patch ]------------------------------- */
+    input_url = data.split("a href=\"")[1].split("\">link")[0];
+    data = Utils.getWebText(input_url);
+    /* --------------------------------------------------------- */
+
+    this.artist = data.split("<ul class=\"comma-list\">")[1]
+        .replace('\n', "").split("</li>")[0]
+        .replace(/(<([^>]+)>)/g, "")
+        .replaceAll('\n', "")
+        .replaceAll('  ', " ");
+    this.title = data.split("<h1><a href=\"/reader/" + gallery_id + ".html\">")[1]
+        .split("</a></h1>")[0];
+    var TagsHtml = data.split("<td>Tags</td>")[1]
+        .split("<ul class=\"tags\">")[1]
+        .split("</ul>")[0]
+        .split("<li><a href=\"");
+    var tags = "";
+    for (var j in TagsHtml) {
+        if (j != 0) {
+            if (j != TagsHtml.length - 1) {
+                tags = tags + TagsHtml[j].split(">")[1].split("<")[0]; + "\n";
+            } else {
+                tags = tags + TagsHtml[j].split(">")[1].split("<")[0];
+            }
+        }
+    }
+    this.tags = tags;
+}
+
+/**
+ * Main function including almost of routines.
+ * @param {string} room 메시지를 받은 방 이름
+ * @param {string} msg 메시지 내용
+ * @param {string} sender 전송자 닉네임
+ * @param {boolean} isGroupChat 단체/오픈채팅 여부
+ * @param {any} replier 응답용 객체. replier.reply("메시지") 또는
+ * replier.reply("방이름","메시지")으로 전송
+ * @param {any} ImageDB ImageDB.getProfileImage(): 전송자의 프로필 이미지를 Base64로 인코딩하여 반환
+ * @param {string} packageName 메시지를 받은 메신저의 패키지 이름
+ * @param {number} threadId 현재 쓰레드의 순번(스크립트별로 따로 매김)
+ * Api,Utils객체에 대해서는 설정의 도움말 참조
+ */
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
     var BlackList = new Array("사용 금지할 채팅방");
-    if (BlackList.indexOf(room)==-1) {
-        if (msg.charAt(1) == "!" && msg.charAt(0) == "h") {
-            try {
-                var number = msg.split("!")[1];
-                var input_url = 'https://hitomi.la/galleries/' + number + ".html";
-                var data = Utils.getWebText(input_url);
-                
-                /* --[ hitomi.la patch ]----------------------- */
-                input_url = data.split("a href=\"")[1];
-                input_url = input_url.split("\">link")[0];
-                data = Utils.getWebText(input_url);
-                /* -------------------------------------------- */
-                
-                var title_before = "<h1><a href=\"/reader/" + number + ".html\">";
-                var title_next = "</a></h1>";
-
-                var artist = data.split("<ul class=\"comma-list\">")[1];
-                artist = artist.replace('\n', "");
-                artist = artist.split("</li>")[0].replace(/(<([^>]+)>)/g, "");
-                var a = data.split(title_before)[1];
-                a = a.split(title_next)[0];
-                replier.reply(a);
-                var i;
-                while (i <= 5) {
-                    artist = artist.replace('\n', "");
-                    artist = artist.replace('  ', " ");
-                    i = i + 1;
-                }
-                var tags = "";
-                var TagsHtml = data.split("<td>Tags</td>")[1];
-                Log.info(TagsHtml);
-                TagsHtml = TagsHtml.split("<ul class=\"tags\">")[1];
-                Log.info(TagsHtml);
-                TagsHtml = TagsHtml.split("</ul>")[0];
-                Log.info(TagsHtml);
-                TagsHtml = TagsHtml.split("<li><a href=\"");
-
-                replier.reply("작가:" + artist);
-                var Tag;
-                for (var j in TagsHtml) {
-                    if (j != 0) {
-                        if (j != TagsHtml.length - 1) {
-                            Log.info(TagsHtml[j]);
-                            Tag = TagsHtml[j].split(">")[1];
-                            Tag = Tag.split("<")[0];
-                            tags = tags + Tag + "\n";
-                        } else {
-                            Log.info(TagsHtml[j]);
-                            Tag = TagsHtml[j].split(">")[1];
-                            Tag = Tag.split("<")[0];
-                            tags = tags + Tag;
-                        }
-                    }
-                }
-                replier.reply(tags);
-            } catch (e) {
-                Log.debug(e + ", " + e.lineNumber);
-                replier.reply("없는 히토미 품번이라꾸~!");
-            }
+    if (BlackList.indexOf(room) == -1 || isGroupChat == false) {
+        if (command(msg)[0] in ["hitomi", "Hitomi", "히토미"]) {
+            var result = new Hitomi(command(msg)[1]);
+            replier.reply(result.title);
+            replier.reply(result.artist);
+            replier.reply(result.tags);
         }
     }
 }
