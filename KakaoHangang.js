49bc0512e9d2621dfa38ac260a222f9fd4806b63
OtakoidTony <36785562+OtakoidTony@users.noreply.github.com>
2019-12-05 20:06:32 +0900

    Update KakaoHangang.js

--- KakaoHangang.js
+++ KakaoHangang.js
@@ -1,7 +1,5 @@
 ﻿function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
     if (msg == ":한강온도") {
-        var data = Utils.getWebText("https://www.wpws.kr/hangang/");
-        data = data.split("</i>")[1].split("</p>")[0].split("도")[0];
-        replier.reply("현재 한강 온도는 " + data + "°C 입니다.");
+        replier.reply("현재 한강 온도는 " + Utils.getWebText("https://www.wpws.kr/hangang/").split("</i>")[1].split("도")[0] + "°C 입니다.");
     }
 }
