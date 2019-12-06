078dc693740415349bf1c171a36395ba7b181783
OtakoidTony <36785562+OtakoidTony@users.noreply.github.com>
2019-12-06 21:06:54 +0900

    Update KakaoMail.js

--- KakaoMail.js
+++ KakaoMail.js
@@ -1,48 +1,22 @@
-﻿var sdcard = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-
-function save(folderName, fileName, str) {
-    var c = new java.io.File(sdcard + "/" + folderName + "/" + fileName);
-    var d = new java.io.FileOutputStream(c);
-    var e = new java.lang.String(str);
-    d.write(e.getBytes());
-    d.close();
-}
-
-function read(folderName, fileName) {
-    var b = new java.io.File(sdcard + "/" + folderName + "/" + fileName);
-    if (!(b.exists())) return null;
-    var c = new java.io.FileInputStream(b);
-    var d = new java.io.InputStreamReader(c);
-    var e = new java.io.BufferedReader(d);
-    var f = e.readLine();
-    var g = "";
-    while ((g = e.readLine()) != null) {
-        f += "\n" + g;
-    }
-    c.close();
-    d.close();
-    e.close();
-    return f.toString();
-}
-
-var folder = new java.io.File(sdcard + "/Teach/");
-folder.mkdirs();
-
-function command(cmd) {
+﻿function command(cmd) {
     var cmd_str = cmd.split(' ')[0];
     var param = cmd.substring(cmd_str.length + 1, cmd.length);
     return [cmd_str, param];
 }
 
-var learned_data = {};
-var learned_data_json = read("Teach", "learned_data.json");
-if (learned_data_json == null) {
-    learned_data = {};
-} else {
-    learned_data = JSON.parse(learned_data_json);
-}
-
+var mail = {};
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
-    
+    if (command(msg)[0] == "/메일쓰기") {
+        var receiver = command(msg)[1].split(':')[0];
+        var contents = command(msg)[1].split(':')[1];
+        if (!(receiver in mail)) {
+            mail[receiver] = [];
+        }
+        mail[receiver].push(sender + "보냄\n\n" + contents);
+    }
+    if (sender in mail) {
+        replier.reply(mail[sender]);
+        delete(mail, sender);
+    }
 }
 
