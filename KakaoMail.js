36b6edbe31230314693100bf4ba063264e0457de
OtakoidTony <36785562+OtakoidTony@users.noreply.github.com>
2019-12-06 21:40:23 +0900

    Update

--- KakaoMail.js
+++ KakaoMail.js
@@ -4,19 +4,23 @@
     return [cmd_str, param];
 }
 
+var allsee = new Array(1000).join(String.fromCharCode(847));
 var mail = {};
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
     if (command(msg)[0] == "/메일쓰기") {
         var receiver = command(msg)[1].split(':')[0];
         var contents = command(msg)[1].split(':')[1];
-        if (!(receiver in mail)) {
-            mail[receiver] = [];
-        }
-        mail[receiver].push(sender + "보냄\n\n" + contents);
+        if (!(receiver in mail)) { mail[receiver] = [receiver + "님에게 메일이 도착했습니다." + allsee];}
+        mail[receiver].push("\n\n" + sender + "가 보낸 메일\n" + contents);
+        replier.reply(receiver + "님에게\n" + sender + "님이\n메일을 보냈어요!")
     }
     if (sender in mail) {
-        replier.reply(mail[sender]);
-        delete(mail[sender]);
+        var send_mail = "";
+        for (var i in mail[sender]) {
+            send_mail = send_mail + mail[sender][i];
+        }
+        replier.reply(send_mail);
+        delete (mail[sender]);
     }
 }
-
