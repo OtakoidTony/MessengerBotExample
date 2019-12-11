81a4816eb43cc94edfec21cc421f1a26d9c3acd7
OtakoidTony <36785562+OtakoidTony@users.noreply.github.com>
2019-12-11 16:13:24 +0900

    Update KakaoDiscord.js

--- KakaoDiscord.js
+++ KakaoDiscord.js
@@ -57,46 +57,83 @@
      * @param {any} channel_id 디스코드 채널 ID
      */
     this.sendMessage = function (sender, message, room, channel_id) {
-        try {
-            var message_json = {
-                "tts": false,
-                "embed": {
-                    "title": sender,
-                    "description": message,
-                    "author": {
-                        "name": room,
-                        "icon_url": "https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium_ov.png"
-                    },
-                    "image": {
-                        "url": "https://developers.kakao.com/assets/img/kakao.png"
+        if (this.isBot) {
+            try {
+                var message_json = {
+                    "tts": false,
+                    "embed": {
+                        "title": sender,
+                        "description": message,
+                        "author": {
+                            "name": room,
+                            "icon_url": "https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium_ov.png"
+                        },
+                        "image": {
+                            "url": "https://developers.kakao.com/assets/img/kakao.png"
+                        }
                     }
-                }
-            };
-            var result = org.jsoup.Jsoup.connect(this.BaseURL + "/v6/channels/" + channel_id + "/messages")
-                .timeout(5000)
-                .ignoreContentType(true)
-                .header("Host", "discordapp.com")
-                .header("Authorization", "Bot " + this.token)
-                .header("User-Agent", "Mozilla/5.0")
-                .header("Cache-Control", "no-cache")
-                .header("Accept-Encoding", "gzip, deflate")
-                .header("Connection", "keep-alive")
-                .header("Cache-Control", "no-cache")
-                .header("Accept", "*/*")
-                .cookies(this.getGatewayBot().cookies())
-                .requestBody(JSON.stringify(message_json))
-                .header("Content-Type", "application/json")
-                .post();
-            return result;
-        } catch (e) {
-            Log.debug(e);
+                };
+                var result = org.jsoup.Jsoup.connect(this.BaseURL + "/v6/channels/" + channel_id + "/messages")
+                    .timeout(5000)
+                    .ignoreContentType(true)
+                    .header("Host", "discordapp.com")
+                    .header("Authorization", "Bearer " + this.token)
+                    .header("User-Agent", "Mozilla/5.0")
+                    .header("Cache-Control", "no-cache")
+                    .header("Accept-Encoding", "gzip, deflate")
+                    .header("Connection", "keep-alive")
+                    .header("Cache-Control", "no-cache")
+                    .header("Accept", "*/*")
+                    .cookies(this.getGatewayBot().cookies())
+                    .requestBody(JSON.stringify(message_json))
+                    .header("Content-Type", "application/json")
+                    .post();
+                return result;
+            } catch (e) {
+                Log.debug(e);
+            }
+        } else {
+            try {
+                var message_json = {
+                    "tts": false,
+                    "embed": {
+                        "title": sender,
+                        "description": message,
+                        "author": {
+                            "name": room,
+                            "icon_url": "https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium_ov.png"
+                        },
+                        "image": {
+                            "url": "https://developers.kakao.com/assets/img/kakao.png"
+                        }
+                    }
+                };
+                var result = org.jsoup.Jsoup.connect(this.BaseURL + "/v6/channels/" + channel_id + "/messages")
+                    .timeout(5000)
+                    .ignoreContentType(true)
+                    .header("Host", "discordapp.com")
+                    .header("Authorization", "Bearer " + this.token)
+                    .header("User-Agent", "Mozilla/5.0")
+                    .header("Cache-Control", "no-cache")
+                    .header("Accept-Encoding", "gzip, deflate")
+                    .header("Connection", "keep-alive")
+                    .header("Cache-Control", "no-cache")
+                    .header("Accept", "*/*")
+                    .cookies(this.getGateway().cookies())
+                    .requestBody(JSON.stringify(message_json))
+                    .header("Content-Type", "application/json")
+                    .post();
+                return result;
+            } catch (e) {
+                Log.debug(e);
+            }
         }
     }
 
     /**
      * 채널 혹은 개인 채팅에서의 메시지를 수신합니다.
      * 만약 길드채널인 경우 이 함수는 VIEW_CHANNEL 권한이 요구됩니다.
-     * @param {string} channel_id
+     * @param {string} channel_id 디스코드 채널 ID
      */
     this.getMessage = function (channel_id) {
         try {
@@ -131,7 +168,7 @@
 
     /**
      * 채널 객체를 반환하는 함수입니다.
-     * @param {string} channel_id
+     * @param {string} channel_id 디스코드 채널 ID
      */
     this.getChannel = function (channel_id) {
         try {
@@ -162,5 +199,56 @@
         } catch (e) {
             Log.debug(e);
         }
+    }
+
+    /**
+     * 채널을 삭제하거나 개인 채팅을 종료(삭제)할 때 사용하는 함수입니다.
+     * @param {any} channel_id 디스코드 채널 ID
+     */
+    this.delectChannel = function (channel_id) {
+        if (this.isBot) {
+            try {
+                var result = org.jsoup.Jsoup.connect(this.BaseURL + "/v6/channels/" + channel_id)
+                    .timeout(5000)
+                    .ignoreContentType(true)
+                    .header("Host", "discordapp.com")
+                    .header("Authorization", "Bot " + this.token)
+                    .header("User-Agent", "Mozilla/5.0")
+                    .header("Cache-Control", "no-cache")
+                    .header("Accept-Encoding", "gzip, deflate")
+                    .header("Connection", "keep-alive")
+                    .header("Cache-Control", "no-cache")
+                    .header("Accept", "*/*")
+                    .cookies(this.getGatewayBot().cookies())
+                    .header("Content-Type", "application/json")
+                    .method(org.jsoup.Connection.Method.DELETE)
+                    .execute();
+                return result;
+            } catch (e) {
+                Log.debug(e);
+            }
+        } else {
+            try {
+                var result = org.jsoup.Jsoup.connect(this.BaseURL + "/v6/channels/" + channel_id)
+                    .timeout(5000)
+                    .ignoreContentType(true)
+                    .header("Host", "discordapp.com")
+                    .header("Authorization", "Bearer " + this.token)
+                    .header("User-Agent", "Mozilla/5.0")
+                    .header("Cache-Control", "no-cache")
+                    .header("Accept-Encoding", "gzip, deflate")
+                    .header("Connection", "keep-alive")
+                    .header("Cache-Control", "no-cache")
+                    .header("Accept", "*/*")
+                    .cookies(this.getGateway().cookies())
+                    .header("Content-Type", "application/json")
+                    .method(org.jsoup.Connection.Method.DELETE)
+                    .execute();
+                return result;
+            } catch (e) {
+                Log.debug(e);
+            }
+        }
+        
     }
 }
