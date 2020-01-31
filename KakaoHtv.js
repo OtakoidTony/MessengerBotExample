34be456a88eef3654fd65ca29e633957444eab72
박현 <36785562+OtakoidTony@users.noreply.github.com>
2020-01-31 20:23:42 +0900

    Update KakaoHtv.js

--- KakaoHtv.js
+++ KakaoHtv.js
@@ -1,12 +1,14 @@
-String.prototype.replaceAll = function(org, dest) {
+String.prototype.replaceAll = function (org, dest) {
     return this.split(org).join(dest);
 }
 
-Utils.getWithUAfromWeb = function(url) {
+Utils.getHanimeJSON = function (url) {
     try {
         var url = new java.net.URL(url);
         var con = url.openConnection();
-        con.setRequestProperty("User-Agent", "\'User-Agent\': \'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\'");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        con.setRequestProperty('X-Directive', 'api');
+
         if (con != null) {
             con.setConnectTimeout(5000);
             con.setUseCaches(false);
@@ -21,54 +23,52 @@ Utils.getWithUAfromWeb = function(url) {
             br.close();
             con.disconnect();
         }
-        return str + "";
+        return JSON.parse(str + "");
     } catch (e) {
         //Log.debug(e);
+        return "error";
     }
 }
 
 
+function Hanime(title) {
+    var input_url = 'https://members.hanime.tv/api/v5/hentai-videos/' + title.replaceAll(' ', '-');
+    var result = Utils.getHanimeJSON(input_url);
+    this.debug = result;
+    if (result != "error") {
+        var video = result.hentai_video;
+        this.title = video.name;
+        this.release = video.released_at.split("T")[0];
+        this.description = video.description.replace(/(<([^>]+)>)/g, "");
+        var tags = [];
+        for (var i in video.hentai_tags) {
+            tags.push(video.hentai_tags[i].text);
+        }
+        this.tags = tags;
+        this.brand = video.brand;
+    }
+}
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
     var BlackList = new Array("사용 금지할 채팅방");
-    if (BlackList.indexOf(room)==-1) {
-        if (msg.charAt(0) == "h" && msg.charAt(1) == "!") {
+    if (BlackList.indexOf(room) == -1) {
+        if (msg.startsWith("hanime:")) {
             try {
-                var title = msg.split("!")[1];
-                var UrlTitle=title.replaceAll(' ', '-');
-                var Url = "https://hanime.tv/videos/hentai/"+UrlTitle;
-                var data = Utils.getWithUAfromWeb(Url);
-                
-                // Title
-                var Title = data.split("tv-title")[1];
-                Title = Title.split("<")[0];
-                Title = Title.split(">")[1];
-                
-                // Brand
-                var Brand = data.split("/browse/brands/")[1];
-                Brand = Brand.split("<")[0];
-                Brand = Brand.split(">")[1];
-                
-                // Release Date
-                var ReleaseDate = data.split("Release Date")[1];
-                ReleaseDate = ReleaseDate.split("hvpimbc-text grey--text")[1];
-                ReleaseDate = ReleaseDate.split("<")[0];
-                ReleaseDate = ReleaseDate.split(">")[1];
-                
-                // Description
-                var Description = data.split("mt-3 mb-0 hvpist-description")[1];                
-                Description = Description.replace(/<p>/gi, "");
-                Description = Description.replace(/<\/p>/gi, "");
-                Description = Description.split("</div>")[0];
-                Description = Description.split(">")[1]; 
-                
-                
-                replier.reply("작품명: "+Title);
-                replier.reply("그룹명: "+Brand);
-                replier.reply("발매일: "+ReleaseDate);
-                replier.reply("줄거리: "+Description);
+                msg = msg.toLowerCase();
+                var title = msg.split(":")[1];
+                var res = new Hanime(title);
+                var tag = "";
+                for (i in res.tags) {
+                    if(i==res.tags.length-1){
+                        tag += res.tags[i];
+                    }else{
+                        tag += res.tags[i] + ', ';
+                    }
+                }
+                replier.reply(res.title + "\n\n발매일: " + res.release + "\n줄거리: " + res.description + "태그: " + tag);
             } catch (e) {
                 Log.debug(e + ", " + e.lineNumber);
-                replier.reply("에러가 발생했습니다.");
+                replier.reply(res.debug);
             }
         }
     }
