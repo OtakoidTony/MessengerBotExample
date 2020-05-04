13df2bb28a475768337409249289c790b6acf3a2
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2020-05-04 15:10:36 +0900

    Update KakaoSenderData.js

--- KakaoSenderData.js
+++ KakaoSenderData.js
@@ -1,53 +1,5 @@
-var sdcard = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
 
-/**
- * 문자열을 파일로 만들어 SD카드에 저장하는 함수
- * @param {string}folderName 저장할 위치
- * @param {string}fileName 저장할 파일의 이름
- * @param {string}str 저장할 문자열 
- */
-function save(folderName, fileName, str) {
-    var c = new java.io.File(sdcard + "/" + folderName + "/" + fileName);
-    var d = new java.io.FileOutputStream(c);
-    var e = new java.lang.String(str);
-    d.write(e.getBytes());
-    d.close();
-}
-
-/**
- * SD카드에 있는 특정 파일을 읽어 문자열로 내보내는 함수
- * @param {string}folderName 파일이 있는 경로명
- * @param {string}fileName 파일명
- */
-function read(folderName, fileName) {
-    var b = new java.io.File(sdcard + "/" + folderName + "/" + fileName);
-    if (!(b.exists())) return null;
-    var c = new java.io.FileInputStream(b);
-    var d = new java.io.InputStreamReader(c);
-    var e = new java.io.BufferedReader(d);
-    var f = e.readLine();
-    var g = "";
-    while ((g = e.readLine()) != null) {
-        f += "\n" + g;
-    }
-    c.close();
-    d.close();
-    e.close();
-    return f.toString();
-}
-
-var folder = new java.io.File(sdcard + "/Kakao_senderData/");
-folder.mkdirs();
-
-var senderData = read(folder, "senderData.json");
-if (senderData == null) {
-    senderData = {};
-} else {
-    senderData = JSON.parse(senderData);
-}
-
-
-Array.prototype.sort_by = function (name, ascending = true) {
+Array.prototype.sort_by = function (name, ascending) {
     if (ascending) {
         return this.sort(function (a, b) {
             if (a[name] > b[name]) {
@@ -57,7 +9,7 @@ Array.prototype.sort_by = function (name, ascending = true) {
                 return -1;
             }
             return 0;
-        })
+        });
     } else {
         return this.sort(function (a, b) {
             if (a[name] > b[name]) {
@@ -67,32 +19,44 @@ Array.prototype.sort_by = function (name, ascending = true) {
                 return 1;
             }
             return 0;
-        })
+        });
     }
-}
+};
 
 Array.prototype.findObjectIndex = function (label, value) {
     for (var i = 0; i < this.length; i++)
         if (this[i][label] == value) return i;
     return -1;
-}
+};
+
 Array.prototype.findObject = function (label, value) {
     if (this.findObjectIndex(label, value) != -1) return this[this.findObjectIndex(label, value)];
     else return null;
+};
+
+
+var folder = new java.io.File(sdcard + "/Kakao_senderData/");
+folder.mkdirs();
+
+var senderData = FileStream.read("sdcard/Kakao_senderData/senderData.json");
+if (senderData == null) {
+    senderData = {};
+} else {
+    senderData = JSON.parse(senderData);
 }
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
-    if (!room in senderData) senderData[room] = [];
+    if (!(room in senderData)) {
+        senderData[room] = [];
+        replier.reply('rojiku created senderData[room] data');
+    }
     if (senderData[room].findObjectIndex('name', sender) == -1) {
-        senderData[room].push({
-            'name': sender,
-            'score': 1
-        })
+        senderData[room].push({'name': sender, 'score': 1});
     } else {
         senderData[room].findObject('name', sender)['score'] += 1;
     }
     if (msg == "call rojiku display ranking system") {
         replier.reply(JSON.stringify(senderData[room].sort_by('score', ascending = false), null, '\t'));
     }
-    save(folder, "senderData.json", JSON.stringify(senderData, null, '\t'));
+    FileStream.write("sdcard/Kakao_senderData/senderData.json", JSON.stringify(senderData, null, '\t'));
 }
