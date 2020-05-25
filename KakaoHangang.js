d1277461db74a15d51e7efddf05434e43c5ac8a9
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2020-05-25 23:31:47 +0900

    Update KakaoHangang.js

--- KakaoHangang.js
+++ KakaoHangang.js
@@ -1,5 +1,3 @@
-﻿function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
-    if (msg == ":한강온도") {
-        replier.reply("현재 한강 온도는 " + Utils.getWebText("https://www.wpws.kr/hangang/").split("</i>")[1].split("<s")[0] + "°C 입니다.");
-    }
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
+    if (msg == ":한강온도") replier.reply("현재 한강 온도는 " + Utils.getWebText("https://www.wpws.kr/hangang/").split("</i>")[1].split("<s")[0] + "°C 입니다.");
 }
