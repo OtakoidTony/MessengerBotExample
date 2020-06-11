c35b85359924f167ac5f7b162fd23414fb725183
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2020-06-11 12:48:40 +0900

    Update KakaoSenderData.js

--- KakaoSenderData.js
+++ KakaoSenderData.js
@@ -49,12 +49,82 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
     if (senderData[room].findObjectIndex('name', sender) == -1) {
         senderData[room].push({
             'name': sender,
-            'score': 1
+            'score': 1,
+            'time': new Date.now()
         });
     } else {
         senderData[room].findObject('name', sender)['score'] += 1;
     }
     var msg_arg = msg.split(' ');
+
+    // 당일 출석한 회원 목록 표시
+    if (msg_arg[0] == "!출석부") {
+        senderData[room].sort_by('score', ascending = false);
+        var output = '\u200b'.repeat(500) + '\n\n';
+        var today = new Date();
+        for (var i = 0; i < senderData[room].length; i++) {
+            user_time = senderData[i].time;
+            if (user_time.getFullYear() == today.getFullYear() && user_time.getMonth() == today.getMonth() && user_time.getDay == today.getDay()) {
+                if (i == senderData[room].length - 1) {
+                    output += 'Name: ' + senderData[room][i].name;
+                } else {
+                    output += 'Name: ' + senderData[room][i].name + '\n\n';
+                }
+            }
+        }
+        replier.reply(output);
+    }
+
+    if (msg_arg[0] == "!랭킹") {
+
+        // 모든 회원에 대한 랭킹 표시
+        if (msg_arg.length == 1) {
+            senderData[room].sort_by('score', ascending = false);
+            var output = '\u200b'.repeat(500) + '\n\n';
+            for (var i = 0; i < senderData[room].length; i++) {
+                user_time = senderData[i].time;
+                output += 'Name: ' + senderData[room][i].name + '\n';
+                output += 'Rank: ' + (i + 1) + "등" + '\n';
+                output += 'Time: ' + user_time.getFullYear() + "년 " + user_time.getMonth() + "월" + user_time.getDay() + "일";
+                if (i == senderData[room].length - 1) {
+                    output += 'Score: ' + senderData[room][i].score;
+                } else {
+                    output += 'Score: ' + senderData[room][i].score + '\n\n';
+                }
+            }
+            replier.reply(output);
+        }
+
+
+        if (msg_arg.length == 3) {
+
+            // 닉네임 검색 기능.
+            // !랭킹 검색 <닉네임의 일부>
+            if (msg_arg[1] == "검색") {
+                var target = msg.substring((msg_arg[0] + ' ' + msg_arg[1] + ' ').length, msg.length);
+                var resultData = [];
+                for (let index = 0; index < senderData[room].length; index++)
+                    if (senderData[room][index].name.indexOf(target) != -1)
+                        resultData.push(senderData[room][index]);
+
+                resultData.sort_by('score', ascending = false);
+                var output = '\u200b'.repeat(500) + '\n\n';
+                for (var i = 0; i < resultData.length; i++) {
+                    user_time = resultData[i].time;
+                    output += 'Name: ' + resultData[i].name + '\n';
+                    output += 'Rank: ' + (i + 1) + "등" + '\n';
+                    output += 'Time: ' + user_time.getFullYear() + "년 " + user_time.getMonth() + "월" + user_time.getDay() + "일";
+                    if (i == resultData.length - 1) {
+                        output += 'Score: ' + resultData[i].score;
+                    } else {
+                        output += 'Score: ' + resultData[i].score + '\n\n';
+                    }
+                }
+                replier.reply(output);
+            }
+        }
+    }
+
     if (msg_arg[0] == "call" || msg_arg[0] == "Call") {
         if (msg_arg[1] == "rojiku") {
             if (msg_arg[2] == "inspect") {
@@ -73,34 +143,34 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
                             if (user_index == -1) {
                                 replier.reply("SYSTEM ALERT:\nIndex Out Of Bounds Exception")
                             } else {
+                                var user_time = senderData[room][user_index].time;
                                 var output = ''
                                 output += 'Name: ' + senderData[room][user_index].name + '\n';
-                                output += 'Time: ' + senderData[room][user_index].score + '\n';
-                                output += 'Rank: ' + (user_index + 1);
+                                output += 'Rank: ' + (user_index + 1) + '\n';
+                                output += 'Time: ' + user_time.getFullYear() + "년 " + user_time.getMonth() + "월" + user_time.getDay() + "일";
+                                output += 'Score: ' + senderData[room][user_index].score;
                                 replier.reply(output);
                             }
                         }
                         if (msg_arg[5] == "search") {
                             var target = msg.substring((msg_arg[0] + ' ' + msg_arg[1] + ' ' + msg_arg[2] + ' ' + msg_arg[3] + ' ' + msg_arg[4] + ' ' + msg_arg[5] + ' ').length, msg.length);
                             var resultData = [];
-                            for (let index = 0; index < senderData[room].length; index++) {
+                            for (let index = 0; index < senderData[room].length; index++)
                                 if (senderData[room][index].name.indexOf(target) != -1)
                                     resultData.push(senderData[room][index]);
-                            }
 
                             resultData.sort_by('score', ascending = false);
                             var output = '\u200b'.repeat(500) + '\n\n';
                             for (var i = 0; i < resultData.length; i++) {
                                 output += 'Name: ' + resultData[i].name + '\n';
                                 if (i == resultData.length - 1) {
-                                    output += 'Time: ' + resultData[i].score;
+                                    output += 'Score: ' + resultData[i].score;
                                 } else {
-                                    output += 'Time: ' + resultData[i].score + '\n\n';
+                                    output += 'Score: ' + resultData[i].score + '\n\n';
                                 }
                             }
                             replier.reply(output);
                         }
-
                     }
                 }
             }
@@ -130,9 +200,9 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
                         for (var i = 0; i < senderData[room].length; i++) {
                             output += 'Name: ' + senderData[room][i].name + '\n';
                             if (i == senderData[room].length - 1) {
-                                output += 'Time: ' + senderData[room][i].score;
+                                output += 'Score: ' + senderData[room][i].score;
                             } else {
-                                output += 'Time: ' + senderData[room][i].score + '\n\n';
+                                output += 'Score: ' + senderData[room][i].score + '\n\n';
                             }
                         }
                         replier.reply(output);
@@ -145,9 +215,9 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
                                 for (var i = 0; i < head.length; i++) {
                                     output += 'Name: ' + head[i].name + '\n';
                                     if (i == head.length - 1) {
-                                        output += 'Time: ' + head[i].score;
+                                        output += 'Score: ' + head[i].score;
                                     } else {
-                                        output += 'Time: ' + head[i].score + '\n\n';
+                                        output += 'Score: ' + head[i].score + '\n\n';
                                     }
                                 }
                                 replier.reply(output);
