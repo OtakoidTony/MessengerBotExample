8740fd2066765a77f44f121a1d27f8fd4dfc104b
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2020-07-25 23:53:32 +0900

    Update KakaoPapago.js

--- KakaoPapago.js
+++ KakaoPapago.js
@@ -1,3 +1,10 @@
+/**
+ * Papago의 번역기능을 이용해 텍스트를 번역한 문자열을 반환하는 함수
+ * @param {String} source 번역될 텍스트의 언어코드
+ * @param {String} target 번역할 언어코드
+ * @param {String} text 번역할 텍스트
+ * @returns {String} 번역 결과
+ */
 papago = function(source, target, text) {
     return JSON.parse(org.jsoup.Jsoup.connect("https://papago.naver.com/apis/n2mt/translate")
         .requestBody('data=' + encodeURIComponent(JSON.stringify({
