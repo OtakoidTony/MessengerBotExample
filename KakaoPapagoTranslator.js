b8f04cc923f258d71aa78a3087bb4094056c542b
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2020-07-26 15:15:09 +0900

    Create KakaoPapagoTranslator.js

---
+++ KakaoPapagoTranslator.js
@@ -0,0 +1,32 @@
+/**
+ * Papago의 번역기능을 이용해 텍스트를 번역한 문자열을 반환하는 함수
+ * @param {String} source 번역될 텍스트의 언어코드
+ * @param {String} target 번역할 언어코드
+ * @param {String} text 번역할 텍스트
+ * @returns {String} 번역 결과
+ */
+function papago(source, target, text) {
+    return JSON.parse(org.jsoup.Jsoup.connect("https://papago.naver.com/apis/n2mt/translate")
+        .requestBody('data=' + encodeURIComponent(JSON.stringify({
+            "source": source,
+            "target": target,
+            "text": text
+        })))
+        .ignoreContentType(true)
+        .post()
+        .text()
+    ).translatedText;
+}
+
+String.prototype.splitInto2Parts = function (chr) {
+    var cmd_str = this.split(chr)[0];
+    return [cmd_str, this.substring(cmd_str.length + 1, this.length)];
+}
+
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
+    param = msg.splitInto2Parts('\n');
+    firstLineParam = param[0].split(' ');
+    if (firstLineParam[0]=="!번역"){
+        replier.reply(papago(firstLineParam[1], firstLineParam[2], param[1]));
+    }
+}
